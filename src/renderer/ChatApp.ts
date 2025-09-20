9f06c0b36d628acc940bcacdaaef5235e4f325b9
suichanwa <augustinnegura746@gmail.com>
2025-09-20 22:29:04 +0300

    uasvj

--- src/renderer/ChatApp.ts
+++ src/renderer/ChatApp.ts
@@ -599,7 +599,7 @@ export class ChatApp implements Component {
         const timestampBlock = `
           <div class="message-time-wrapper">
             ${timestampHtml}
-            <span class="timestamp-hint">shoot tim</span>
+            <span class="timestamp-hint">shoot time</span>
           </div>
         `;
 
